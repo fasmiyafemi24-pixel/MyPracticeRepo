989783d7d5fcbd0d547eece185adffe94b24b3a9
fasmiyafemi24-pixel <fasmiyafemi24@gmail.com>
2026-05-17 09:47:44 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,18 @@
+package Assignment;
+
+class Employee{
+	int id;
+	String name;
+	Employee(str){
+		System.out.println("")
+	}
+}
+
+public class employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
