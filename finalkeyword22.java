02aec1a0b204a34ebb3357940aecb5da20a5a8ef
fasmiyafemi24-pixel <fasmiyafemi24@gmail.com>
2026-06-08 15:54:11 +0530

    Add files via upload

---
+++ finalkeyword22.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+class GovernmentRules
+{
+	final int MAX_WORKING_HOURS=8;
+}
+
+public class finalkeyword22 {
+
+	public static void main(String[] args) {
+		
+		GovernmentRules obj = new GovernmentRules();
+		
+		
+		System.out.println(obj.MAX_WORKING_HOURS);
+		
+	}
+
+}
