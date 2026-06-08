9ed6f1b8075fe2efcb677300fc45e4a2a75e5074
fasmiyafemi24-pixel <fasmiyafemi24@gmail.com>
2026-06-08 18:00:03 +0530

    Add files via upload

---
+++ School25.java
@@ -0,0 +1,28 @@
+package Assignment;
+
+class School
+{
+	String name="Delhi public school";
+	
+	School()
+	{
+		System.out.println("School name is "+ name);
+	}
+	void disply()
+	{
+		System.out.println("This school is based out of Kolkata");
+	}
+	
+}
+
+
+public class School25 {
+
+	public static void main(String[] args) {
+		
+		School obj=new School();
+		obj.disply();
+		
+	}
+
+}
